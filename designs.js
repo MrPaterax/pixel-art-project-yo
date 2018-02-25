b7f82c76be2a4d67f1718a90118842190e4f0236
MrPaterax <patriciotapia6@gmail.com>
2018-02-24 23:57:08 -0500

    what it do

---
+++ designs.js
@@ -0,0 +1,26 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid() {
+
+var grid_width = $("input[name=width]").val();
+var grid_height = $("input[name=height]").val();
+$("table").remove();
+$(".design").append("<table> </table>");
+for (var i = 0; i < grid_height; i++) {
+	$("table").append("<tr> </tr>");
+		for (var x = 0; x < grid_width; x++) {
+			$("table").append("<td> </td>");
+		}
+	}
+}
+
+$("form").submit(function() {
+	event.preventDefault();
+	makeGrid();
+});
+
+$("body").on("mouseover", "td", function() {
+	$(this).css("background-color", $("input[type=color]").val());
+	});
